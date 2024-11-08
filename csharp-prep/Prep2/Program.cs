10a89177d54296b5b19f3e246bd201e5951bae1b
IsaiasPalaco <isaiaspalaco@gmail.com>
2024-11-09 00:28:33 +0200

    console return all grades according to its percentage

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("C");
+        }
+        else 
+        {
+            Console.WriteLine("F");
+        }
     }
 }
