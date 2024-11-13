5d32e71d4b81c2b27a06c408a1c19f37ce477b9f
IsaiasPalaco <isaiaspalaco@gmail.com>
2024-11-13 02:39:48 +0200

    finished part four

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0)
+            {
+                break;
+            }
+
+            numbers.Add(input);
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        int largest = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        
+        Console.WriteLine("");
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
