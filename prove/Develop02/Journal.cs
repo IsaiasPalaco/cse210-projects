b0fa5ee66767a8f1ce8ced53978a78d467f6028e
IsaiasPalaco <isaiaspalaco@gmail.com>
2024-11-21 02:12:07 +0200

    team work w02

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal()
+{
+    List <Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+
+    {
+        
+    }
+}
